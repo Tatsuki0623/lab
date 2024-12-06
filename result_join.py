8720ac71b7e4a23788d02e73338867b879873d5c
Tatsuki0623 <yuyuno918@outlook.jp>
2024-12-06 17:08:52 +0900

    Comprehensive Data

--- result_join.py
+++ result_join.py
@@ -10,27 +10,24 @@ for target_sub_dir in top_dir_names:
     sub_dir_names = [f for f in os.listdir(target_dir_path) if os.path.isdir(os.path.join(target_dir_path, f))]
     
     for target_dir in sub_dir_names:
-        
         try:
             target_file_path = target_dir_path + target_dir + "/high_cooncent_check.csv"
             df = pd.read_csv(target_file_path, index_col = 0).T
             df.rename(index = {"0": target_sub_dir + "_" + target_dir}, inplace = True)
         except FileNotFoundError:
-            try:
-                target_file_path = target_dir_path + target_dir + "/high_concent_check.csv"
-                df = pd.read_csv(target_file_path, index_col = 0).T
-                df.rename(index = {"0": target_sub_dir + "_" + target_dir}, inplace = True)
-            except FileNotFoundError:
-                df = None
-        print(df)
-        if df is not None:
-            continue
+            target_file_path = target_dir_path + target_dir + "/high_concent_check.csv"
+            df = pd.read_csv(target_file_path, index_col = 0).T
+            df.rename(index = {"0": target_sub_dir + "_" + target_dir}, inplace = True)
+        except Exception as e:
+            print(e)
+
         df_list.append(df)
 
 new_df = pd.concat(df_list)
 new_df.drop(columns = ['予測高濃度出現回数', '予測高濃度追跡', '予測高濃度追跡率', '適合率'], inplace = True)
+new_df.to_csv("out_data/test_data/test.csv", encoding="sjis")
 
-locates = set([i.split("_")[0] for i in target_sub_dir])
+locates = set([i.split("_")[0] for i in sub_dir_names if (len(i.split("_")) == 2)])
 for locate in locates:
     filtered_df = new_df[new_df.index.str.contains(locate)]
     filtered_df.sort_index(inplace=True)
